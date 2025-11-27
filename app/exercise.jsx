79acfc443aa30980583bb3bb296c67699263c483
Dantesos33 <dantesos33@gmail.com>
2025-11-27 19:06:14 +0500

    Home Screen

---
+++ app/exercise.jsx
@@ -0,0 +1,28 @@
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, View } from "react-native";
+import exercises from "../assets/data/exercises.json";
+import ExerciseListItem from "../components/ExerciseListItem";
+
+export default function ExerciseScreen() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={exercises}
+        contentContainerStyle={{ gap: 5 }}
+        keyExtractor={(item, index) => item.name + index}
+        renderItem={({ item }) => <ExerciseListItem item={item} />}
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "ghostwhite",
+    justifyContent: "center",
+    padding: 10,
+  }
+});
--- app/index.jsx
+++ app/index.jsx
@@ -1,19 +1,40 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { FlatList, StyleSheet, View } from "react-native";
-import exercises from "../assets/data/exercises.json";
-import ExerciseListItem from "../components/ExerciseListItem";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
-      <FlatList
-        data={exercises}
-        contentContainerStyle={{ gap: 5 }}
-        keyExtractor={(item, index) => item.name + index}
-        renderItem={({ item }) => <ExerciseListItem item={item} />}
-      />
-
       <StatusBar style="auto" />
+      <Image
+        style={{ height: "100%", width: "100%", position: "absolute" }}
+        source={require("../assets/images/welcome.png")}
+      />
+      <LinearGradient
+        colors={["transparent", "#18181b"]}
+        style={{ width: wp(100), height: hp(70), display: "flex", justifyContent: "flex-end", paddingBottom: 50, marginVertical: "28px" }}
+        start={{x: 0.5, y: 0}}
+        end={{x: 0.5, y: 0.8}}
+      >
+        <View style={{display: "flex", alignItems: "center"}}>
+          <Text style={{ color: "white", fontSize: hp(5), fontWeight: "bold", marginBottom: 20, textAlign: "center" }}>
+            Welcome to <Text style={{color: "rgb(244, 63, 94)"}}>FitTrack</Text>
+          </Text>
+        </View>
+
+        <View>
+          <TouchableOpacity onPress={() => router.push("exercise")} style={{height: hp(7), width: wp(80), backgroundColor: "rgb(244, 63, 94)", borderRadius: 50, display: "flex", justifyContent: "center", alignItems: "center", alignSelf: "center", borderWidth: 2, borderColor: "white"}}>
+            <Text style={{color: "white", fontWeight: "bold", fontSize: hp(3)}}>Get Started</Text>
+          </TouchableOpacity>
+        </View>
+      </LinearGradient>
     </View>
   );
 }
@@ -22,7 +43,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "ghostwhite",
-    justifyContent: "center",
-    padding: 10,
-  }
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
 });
